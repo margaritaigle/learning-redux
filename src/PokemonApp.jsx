6690934a38e817bb06f3923ac08d1def62b58557
Margarita Iglesias <margaritaigle99@gmail.com>
2022-12-04 16:56:21 -0300

    adding thunks and axios

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,14 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemons } from "./store/slices/pokemon";
+
 export const PokemonApp = () => {
+  const dispatch = useDispatch();
+
+  const { isLoading, pokemons, page } = useSelector((state) => state.pokemons);
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, []);
+
   return (
     <>
       <h1>PokemonApp</h1>
       <hr />
+      <span>Loading: {isLoading ? "True" : "False"}</span>
 
       <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+        {pokemons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
       </ul>
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next page
+      </button>
     </>
   );
 };
--- src/store/slices/pokemon/pokemonSlice.js
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -12,11 +12,14 @@ export const pokemonSlice = createSlice({
          state.isLoading = true;
       },
       setPokemons: (state, action) => {
-        
+         state.isLoading = false;
+         state.page = action.payload.page;
+         state.pokemons = action.payload.pokemons
       }
    }
 });
 
 
 // Action creators are generated for each case reducer function
-export const {  startLoadingPokemons } = pokemonSlice.actions;
+export const {  startLoadingPokemons, setPokemons } = pokemonSlice.actions;
+
